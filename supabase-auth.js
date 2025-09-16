138c8a38d305ce890bcc8ccf8bc4b962f80e13b7
rakit1 <dmitriy0905q@gmail.com>
2025-09-17 00:22:24 +0200

    Update supabase-auth.js

--- supabase-auth.js
+++ supabase-auth.js
@@ -1,7 +1,7 @@
 // === Настройка Supabase ===
 // Замени на свои реальные данные из https://supabase.com/dashboard -> Project settings -> API
-const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://egskxyxgzdidfbxhjaud.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnc2t4eXhnemRpZGZieGhqYXVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNTA2MDcsImV4cCI6MjA3MzYyNjYwN30.X60gkf8hj0YEKzLdCFOOXRAlfDJ2AoINoJHY8qPeDFw";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
