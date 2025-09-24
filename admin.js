be836b873e2abc26fc64da3b3e09c8d56ff50b32
rakit1 <dmitriy0905q@gmail.com>
2025-09-24 04:28:11 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -9,9 +9,10 @@ class AdminPage {
     }
 
     init() {
-        this.authManager.auth.onAuthStateChanged(user => {
-            if (user && this.authManager.user?.role === 'Администратор') {
-                this.user = this.authManager.user;
+        document.addEventListener('userStateReady', (event) => {
+            const user = event.detail;
+            if (user?.role === 'Администратор') {
+                this.user = user;
                 this.adminContainer.style.display = 'block';
                 this.fetchAllTickets();
             } else if (user) {
@@ -36,16 +37,15 @@ class AdminPage {
                 this.ticketsListContainer.innerHTML = snapshot.docs.map(doc => {
                     const ticket = doc.data();
                     const author = profiles.get(ticket.user_id) || { username: 'Неизвестный' };
-                    const date = ticket.created_at ? new Date(ticket.created_at.toDate()).toLocaleDateString('ru-RU', {day: 'numeric', month: 'long', year: 'numeric'}) : 'неизвестно';
-                    const statusIndicator = ticket.is_closed ? '<span class="ticket-status closed">Закрыт</span>' : '<span class="open-ticket-btn">Ответить</span>';
-                    return `<a href="ticket.html?id=${doc.id}" class="ticket-card-link"><div class="ticket-card"><div><span class="ticket-id">Тикет #${doc.id}</span><div class="ticket-author">От: ${author.username}</div><p class="ticket-description">${ticket.description}</p></div><div class="ticket-footer"><span class="ticket-date">Создано: ${date}</span>${statusIndicator}</div></div></a>`;
+                    const date = new Date(ticket.created_at.toDate()).toLocaleDateString('ru-RU', {day: 'numeric', month: 'long', year: 'numeric'});
+                    const status = ticket.is_closed ? '<span class="ticket-status closed">Закрыт</span>' : '<span class="open-ticket-btn">Ответить</span>';
+                    return `<a href="ticket.html?id=${doc.id}" class="ticket-card-link"><div class="ticket-card"><div><span class="ticket-id">Тикет #${doc.id}</span><div class="ticket-author">От: ${author.username}</div><p class="ticket-description">${ticket.description}</p></div><div class="ticket-footer"><span class="ticket-date">Создано: ${date}</span>${status}</div></div></a>`;
                 }).join('');
             } else {
                 this.ticketsListContainer.innerHTML = '<p class="no-tickets-message">Активных обращений нет.</p>';
             }
         } catch (error) {
             console.error("Ошибка загрузки всех тикетов:", error);
-            this.ticketsListContainer.innerHTML = `<p class="no-tickets-message error">Не удалось загрузить обращения: ${error.message}</p>`;
         }
     }
 }
